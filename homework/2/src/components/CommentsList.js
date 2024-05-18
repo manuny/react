929a0109ed968a0b990361c1838b32acdc5cde50
Vasileva Masha <Vasilevamasha1982@gmail.com>
2024-05-18 14:09:06 +0300

    2

---
+++ homework/2/src/components/CommentsList.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const CommentsList = () => {
+  const [comments, setComments] = useState([
+    { id: 1, text: "Это первый комментарий" },
+    { id: 2, text: "Это второй комментарий" },
+    { id: 3, text: "Это третий комментарий" },
+  ]);
+
+  const deleteComment = (id) => {
+    setComments(comments.filter((comment) => comment.id !== id));
+  };
+
+  return (
+    <div>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>
+            {comment.text}
+            <button onClick={() => deleteComment(comment.id)}>
+              {" "}
+              Удалить комментарий{" "}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CommentsList;
