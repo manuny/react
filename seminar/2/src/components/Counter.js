d69d6d3d203497ba15c55966beae78218e0eb583
Vasileva Masha <Vasilevamasha1982@gmail.com>
2024-05-18 12:30:02 +0300

    2

---
+++ seminar/2/src/components/Counter.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  // Инициализация состояния счётчика со значением 0
+
+  const [count, setCount] = useState(0);
+
+  // Функция для увеличения счётчика на 1
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={incrementCount}> Увеличить на 1 </button>
+    </div>
+  );
+};
+export default Counter;
